76dc43a633f5d4a91f73f82621d9f2b52bd34efe
Benjamin <bengscribner@gmail.com>
2025-12-02 14:59:21 +0100

    small card changes

--- Assets/Scripts/CardScripts/CardScrub.cs
+++ Assets/Scripts/CardScripts/CardScrub.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class CardScrub : ScriptableObject
 {
-    public string name;
+    public string cardName;
+    public CardType cardType;
     public string description;
 
     public Sprite cardArtwork;
 
     public int Attack;
 
+    public enum CardType
+    {
+        Attack,
+        Effect,
+        Instant,
+    }
 }
