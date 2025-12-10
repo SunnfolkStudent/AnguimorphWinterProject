67512b7c9d43f9a79aee56dcdcb253d94df821c8
Lacrimosa-GH <lacrimosa1457@gmail.com>
2025-12-10 10:28:04 +0100

    Added a sapce for sondre's bonus challenge

---
+++ Assets/Scripts/HandScripts/Sondre/CopyCardHandController.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TouchPhase = UnityEngine.TouchPhase;
+
+public class CopyCardHandController : MonoBehaviour
+{
+    //public Card SOcard;
+    public List<GameObject> cards;
+    public DeckList Deck;
+    
+    [SerializeField] private int handMaximum = 3;
+    [SerializeField] private GameObject CardPrefab;
+    
+    //private int amount = 3;
+
+    public float spacing = 3f;
+    private Vector3 touchPosWorld;
+    TouchPhase phaseEnded = TouchPhase.Ended;
+
+    private void Start()
+    {
+        DrawCards();
+        CardHandPositioning();
+       
+    }
+
+    private void Update()
+    {
+        
+//reset position
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            for (int i = 0; cards.Count > i; i++)
+            {
+                cards[i].transform.position = new Vector2(0, 0);
+            }
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            //DrawCards();
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == phaseEnded)
+            {
+                if (Camera.main != null)
+                {
+                   touchPosWorld = Camera.main.ScreenToWorldPoint(touch.position); Vector2 touchPosWorld2D = new  Vector2(touchPosWorld.x, touchPosWorld.y);
+                                   
+                   RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+                   if (hitInformation.collider != null)
+                   {
+                       GameObject SelectedCard = hitInformation.transform.gameObject;
+                       
+                       Debug.Log("Touched "+ SelectedCard.transform.name);
+                       CardSelected(SelectedCard = SelectedCard.transform.gameObject);
+                   } 
+                }
+                
+            }
+            
+        }
+    }
+
+    void DrawCards()
+    {
+       /* int randCard = UnityEngine.Random.Range(0,Deck.deckList.Count);
+        Debug.Log(randCard);
+        if (Deck.deckList[randCard] != null)
+        {
+            Debug.Log("passed not null");
+            for (int i = 0; i < handMaximum; i++)
+            {
+                
+                cards.Add(Deck.deckList[randCard]);
+                CardHandPositioning();
+                var spawnedCard = Instantiate(CardPrefab);
+                
+                spawnedCard.GetComponent<AttackCardDisplay>().card = SOcard;
+            }
+            
+        }*/
+        
+        
+    }    
+    void CardSelected(GameObject SelectedCard)
+        {
+            SelectedCard.transform.position += new Vector3(0, 2, 0);
+        }
+     void CardHandPositioning()
+    {
+        for (int i = 0; cards.Count > i; i++)
+        {
+            //Sondres Demo for advanced card interface. WIP*
+            //double SetPosition = 1 / (1 + Mathf.Pow(math.E, - cards.Length * 2.5f * (i / amount - 0.5f)));
+          //float xAxis = (float)SetPosition;
+          //xAxis /= i;
+          //cards[i].transform.position = new Vector2(xAxis, -2);
+
+          
+          float xAxis = i * spacing;
+          cards[i].transform.position = new Vector2(xAxis - 2, -2);
+          
+        }
+    }
+
+    
+}
