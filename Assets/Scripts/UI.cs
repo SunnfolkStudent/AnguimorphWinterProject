4ffddb716a3f47e7576d8e41be7187960f6d2342
Benjamin <bengscribner@gmail.com>
2025-12-12 11:25:33 +0100

    added enemy sprite randomixer

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,13 +1,18 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
     private List<GameObject> children = new List<GameObject>();
-
+    private List<Sprite> sprites;
+    
+    public GameObject enemySprite;
     private void Start()
     {
         AddDescendants(gameObject.transform, children);
+        sprites = Resources.LoadAll<Sprite>("EnemySprites").ToList();
+        enemySprite.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
     }
     public void QuitButton()
     {
