92c20e22a3b7ccfdb95ff572ef7cb3de9a229488
Benjamin <bengscribner@gmail.com>
2025-12-13 01:19:59 +0100

    made better soundfx system and added soundfx to win screen

--- Assets/Scripts/WinLoseScripts/DeathScreen.cs
+++ Assets/Scripts/WinLoseScripts/DeathScreen.cs
@@ -4,6 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class DeathScreen : MonoBehaviour
 {
+    [SerializeField] private AudioClip WinFXClip;
+    [SerializeField] private AudioClip LoseFXClip;
+    [SerializeField] private AudioClip[] MarkusFXClip;
+    
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "WinScene")
+        {
+            SoundFXManager.instance.PlayRandomSoundFXClip(MarkusFXClip, transform, 1f);
+            SoundFXManager.instance.PlaySoundFXClip(WinFXClip, transform, .2f);
+        }
+
+        else
+        {
+            SoundFXManager.instance.PlaySoundFXClip(LoseFXClip, transform, 1f);
+        }
+    }
+    
     public void LoadSceneByName()
     {
         SceneManager.LoadScene("Frederik");
